acbad5882e2512b1d0fffee73af25ef4bb891abf
Jason Mullins <jason.mullins@hatchpad.io>
2016-09-13 20:09:53 -0400

    added new possible fields for the users.identity method

--- src/main/java/com/github/seratch/jslack/api/methods/response/users/UsersIdentityResponse.java
+++ src/main/java/com/github/seratch/jslack/api/methods/response/users/UsersIdentityResponse.java
@@ -1,6 +1,7 @@
 package com.github.seratch.jslack.api.methods.response.users;
 
 import com.github.seratch.jslack.api.methods.SlackApiResponse;
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
@@ -17,10 +18,24 @@ public class UsersIdentityResponse implements SlackApiResponse {
     public static class User {
         private String name;
         private String id;
+        private String email;
+        @SerializedName("image_24")
+        private String image24;
+        @SerializedName("image_32")
+        private String image32;
+        @SerializedName("image_48")
+        private String image48;
+        @SerializedName("image_72")
+        private String image72;
+        @SerializedName("image_192")
+        private String image192;
+        @SerializedName("image_512")
+        private String image512;
     }
 
     @Data
     public static class Team {
+        private String name;
         private String id;
     }
 }
