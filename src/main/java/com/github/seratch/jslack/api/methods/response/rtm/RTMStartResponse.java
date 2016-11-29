8b737b9deecb2893df03930461b6799a0db01a3d
Mike Kobit <mkobit@gmail.com>
2016-11-28 23:28:11 -0600

    Add in a request options and response data for `rtm.start` method (#9)
    GH: https://github.com/seratch/jslack/issues/6

--- src/main/java/com/github/seratch/jslack/api/methods/response/rtm/RTMStartResponse.java
+++ src/main/java/com/github/seratch/jslack/api/methods/response/rtm/RTMStartResponse.java
@@ -1,8 +1,18 @@
 package com.github.seratch.jslack.api.methods.response.rtm;
 
 import com.github.seratch.jslack.api.methods.SlackApiResponse;
+import com.github.seratch.jslack.api.model.Channel;
+import com.github.seratch.jslack.api.model.Group;
+import com.github.seratch.jslack.api.model.Im;
+import com.github.seratch.jslack.api.model.Team;
+import com.github.seratch.jslack.api.model.User;
 import lombok.Data;
 
+import java.util.List;
+
+/**
+ * @see <a href="https://api.slack.com/methods/rtm.start">rtm.start</a>
+ */
 @Data
 public class RTMStartResponse implements SlackApiResponse {
 
@@ -11,4 +21,10 @@ public class RTMStartResponse implements SlackApiResponse {
     private String error;
 
     private String url;
+    private User self;
+    private Team team;
+    private List<User> users;
+    private List<Channel> channels;
+    private List<Group> groups;
+    private List<Im> ims;
 }
