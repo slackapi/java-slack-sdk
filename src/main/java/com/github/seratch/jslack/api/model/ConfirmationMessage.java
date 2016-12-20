2c1930a2d68c8782f7a6dbef10f00f5c725c35ee
Narasimha Prasad <nprasad@thoughtworks.com>
2016-12-20 15:17:02 +0530

    Adding actions and confirmation message to attachments

---
+++ src/main/java/com/github/seratch/jslack/api/model/ConfirmationMessage.java
@@ -0,0 +1,15 @@
+package com.github.seratch.jslack.api.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ConfirmationMessage {
+    public static final String OKAY = "Okay";
+    public static final String DISMISS = "Cancel";
+    private String title;
+    private String text;
+    private String ok_text = OKAY;
+    private String dismiss_text = DISMISS;
+}
