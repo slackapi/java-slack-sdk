88fdd4f3bbe3ac10d1b404901e36343f816e4823
Joel Toby <joeltoby@gmail.com>
2017-10-06 13:09:49 +0100

    Add support for message menus
    - Add new attachment option Enum for easy switch between button and menu
    option types

--- src/main/java/com/github/seratch/jslack/api/model/Action.java
+++ src/main/java/com/github/seratch/jslack/api/model/Action.java
@@ -6,15 +6,41 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.annotations.SerializedName;
+
 @Data
 @Builder
 public class Action {
-    public static final String BUTTON = "button";
+    /**
+     * Represents the type of action (e.g Message button or message menu)
+     * @see <a href="https://api.slack.com/interactive-messages#interaction_types">Interaction Types</a>
+     */
+    public enum Type {
+      
+      /**
+       * @see <a href="https://api.slack.com/docs/message-buttons">Message button</a>
+       */
+      @SerializedName("button")
+      BUTTON ("button"),
+      
+      /**
+       * @see <a href="https://api.slack.com/docs/message-menus">Message menus</a>
+       */
+      @SerializedName("select")
+      SELECT ("select");
+      
+      private final String value;
+      Type(String value) {
+        this.value = value;
+      }
+      
+      public String value() { return value; }
+    }
 
     private String name;
     private String text;
     private String style;
-    @Builder.Default private String type = BUTTON;
+    @Builder.Default private Type type = Type.BUTTON;
     private String value;
     private Confirmation confirmation;
     private List<Option> options = new ArrayList<>();
