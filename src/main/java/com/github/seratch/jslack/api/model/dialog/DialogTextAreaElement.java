b2a3e086dfb5109dc924e0f83f9875e80eb92949
Joel Toby <joeltoby@gmail.com>
2017-10-22 12:56:26 +0100

    Correct indentation

--- src/main/java/com/github/seratch/jslack/api/model/dialog/DialogTextAreaElement.java
+++ src/main/java/com/github/seratch/jslack/api/model/dialog/DialogTextAreaElement.java
@@ -14,58 +14,58 @@ import lombok.Data;
 @Data
 @Builder
 public class DialogTextAreaElement implements DialogElement {
-  
-  /**
-   * Label displayed to user. Required. No more than 24 characters.
-   */
-  private String label;
-  
-  /**
-   * Name of form element. Required. No more than 300 characters.
-   */
-  private String name;
-  
-  /**
-   * Type of element.  For a textarea, the type is always {@code textarea} . It's required.
-   * 
-   * @see <a href="https://api.slack.com/dialogs#elements">Dialog form elements</a>
-   */
-  private final String type = "textarea";
-  
-  /**
-   * A default value for this field.  Up to 500 characters.
-   */
-  String value;
-  
-  /**
-   * A string displayed as needed to help guide users in completing the element.
-   * 150 character maximum.
-   */
-  private String placeholder;
-  
-  /**
-   * Provide {@code true} when the form element is not required. By default, 
-   * form elements are required.
-   */
-  boolean optional;
-  
-  /**
-   * Maximum input length allowed for element. 0-500 characters. Defaults to 150.
-   */
-  public int maxLength;
-  
-  /**
-   * Minimum input length allowed for element. 1-500 characters. Defaults to 0.
-   */
-  int minLength;
-  
-  /**
-   * Helpful text provided to assist users in answering a question. Up to 150 characters.
-   */
-  String hint;
-  
-  /**
-   * Subtype for this text type element (e.g. Number)
-   */
-  DialogSubType subtype;
+
+    /**
+     * Label displayed to user. Required. No more than 24 characters.
+     */
+    private String label;
+
+    /**
+     * Name of form element. Required. No more than 300 characters.
+     */
+    private String name;
+
+    /**
+     * Type of element.  For a textarea, the type is always {@code textarea} . It's required.
+     * 
+     * @see <a href="https://api.slack.com/dialogs#elements">Dialog form elements</a>
+     */
+    private final String type = "textarea";
+
+    /**
+     * A default value for this field.  Up to 500 characters.
+     */
+    String value;
+
+    /**
+     * A string displayed as needed to help guide users in completing the element.
+     * 150 character maximum.
+     */
+    private String placeholder;
+
+    /**
+     * Provide {@code true} when the form element is not required. By default, 
+     * form elements are required.
+     */
+    boolean optional;
+
+    /**
+     * Maximum input length allowed for element. 0-500 characters. Defaults to 150.
+     */
+    public int maxLength;
+
+    /**
+     * Minimum input length allowed for element. 1-500 characters. Defaults to 0.
+     */
+    int minLength;
+
+    /**
+     * Helpful text provided to assist users in answering a question. Up to 150 characters.
+     */
+    String hint;
+
+    /**
+     * Subtype for this text type element (e.g. Number)
+     */
+    DialogSubType subtype;
 }
