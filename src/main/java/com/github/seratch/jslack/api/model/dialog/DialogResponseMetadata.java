6570bfc4ded3ccbcbde16662e473787a6d502ddb
Louis Pré <louis@prefamily.net>
2018-01-30 18:25:48 -0800

    Adding error metadata to DialogOpenResponse (#58)
    * Adding error metadata to DialogOpenResponse
    * Renaming errorMetadata to DialogResponseMetadata
    * Moving DialogResponseMetadata to its rightful package

---
+++ src/main/java/com/github/seratch/jslack/api/model/dialog/DialogResponseMetadata.java
@@ -0,0 +1,12 @@
+package com.github.seratch.jslack.api.model.dialog;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DialogResponseMetadata {
+
+    private List<String> messages;
+
+}
