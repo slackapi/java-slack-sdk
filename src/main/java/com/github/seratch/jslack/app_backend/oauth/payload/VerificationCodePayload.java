7b37a6c1f4e1696d639bef79585fa3abd141c26a
Kazuhiro Sera <seratch@gmail.com>
2019-04-23 08:00:39 +0900

    Add error parameter to VerificationCodePayload

--- src/main/java/com/github/seratch/jslack/app_backend/oauth/payload/VerificationCodePayload.java
+++ src/main/java/com/github/seratch/jslack/app_backend/oauth/payload/VerificationCodePayload.java
@@ -4,11 +4,15 @@ import lombok.Data;
 
 import java.util.Map;
 
+/**
+ * https://api.slack.com/docs/oauth
+ */
 @Data
 public class VerificationCodePayload {
 
     private String code;
     private String state;
+    private String error; // "access_denied"
 
     /**
      * Extracts code and state from Map object. This method is supposed to be used in AWS lambda functions.
@@ -18,6 +22,7 @@ public class VerificationCodePayload {
         VerificationCodePayload payload = new VerificationCodePayload();
         payload.setCode(queryStringParameters.get("code"));
         payload.setState(queryStringParameters.get("state"));
+        payload.setError(queryStringParameters.get("error"));
         return payload;
     }
 
--- src/test/java/com/github/seratch/jslack/app_backend/oauth/payload/VerificationCodePayloadTest.java
+++ src/test/java/com/github/seratch/jslack/app_backend/oauth/payload/VerificationCodePayloadTest.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class VerificationCodePayloadTest {
@@ -18,6 +19,17 @@ public class VerificationCodePayloadTest {
         VerificationCodePayload payload = VerificationCodePayload.from(queryParameters);
         assertThat(payload.getCode(), is("123"));
         assertThat(payload.getState(), is("random"));
+        assertThat(payload.getError(), is(nullValue()));
+    }
+
+    @Test
+    public void error() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("error", "access_denied");
+        VerificationCodePayload payload = VerificationCodePayload.from(queryParameters);
+        assertThat(payload.getCode(), is(nullValue()));
+        assertThat(payload.getState(), is(nullValue()));
+        assertThat(payload.getError(), is("access_denied"));
     }
 
 }
