e877693e06b9c4777d54f132de8170bf1f3ac547
Kazuhiro Sera <seratch@gmail.com>
2019-04-26 17:30:53 +0900

    Refactor TS types generator script

--- scripts/generate-typescript-types.rb
+++ scripts/generate-typescript-types.rb
@@ -8,6 +8,20 @@ require 'open3'
 index_file = __dir__ + '/../typescript-types/index.ts'
 File.truncate(index_file, 0)
 
+def write(root_class_name, json_path, typedef_filepath, input_json)
+  cmd = "quicktype --just-types --all-properties-optional --acronym-style original -t #{root_class_name} -l ts -o #{typedef_filepath}"
+  puts "Generating #{root_class_name} from #{json_path}"
+  Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+    stdin.write(input_json)
+  end
+end
+
+def append_to_index_ts(root_class_name, index_file, dir)
+  File.open(index_file, 'a') do |index_f|
+    index_f.puts("export { #{root_class_name} } from './#{dir}/#{root_class_name}';")
+  end
+end
+
 # web-api
 Dir.glob(__dir__ + '/../typescript-types/source/web-api/*').each do |json_path|
   File.open(json_path) do |json_file|
@@ -26,81 +40,33 @@ Dir.glob(__dir__ + '/../typescript-types/source/web-api/*').each do |json_path|
     end
     root_class_name << 'Response'
 
-    typedef_file = __dir__ + "/../typescript-types/web-api/#{root_class_name}.d.ts"
-    cmd = "quicktype --just-types --all-properties-optional --acronym-style original -t #{root_class_name} -l ts -o #{typedef_file}"
+    typedef_filepath = __dir__ + "/../typescript-types/web-api/#{root_class_name}.d.ts"
     input_json = json_file.read
-    puts "Generating #{root_class_name} from #{json_path}"
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-      stdin.write(input_json)
-    end
-
-    File.open(index_file, 'a') do |index_f|
-      index_f.puts("export { #{root_class_name} } from './web-api/#{root_class_name}';")
-    end
-
+    write(root_class_name, json_path, typedef_filepath, input_json)
+    append_to_index_ts(root_class_name, index_file, 'web-api')
   end
 end
 
 # events-api
 Dir.glob(__dir__ + '/../typescript-types/source/events-api/*').each do |json_path|
   File.open(json_path) do |json_file|
-    root_class_name = 'Events'
-    prev_c = nil
     filename = json_path.split('/').last.gsub(/\.json$/, '')
-    filename.split('').each do |c|
-      if prev_c.nil? || prev_c == '.'
-        root_class_name << c.upcase
-      elsif c == '.'
-        # noop
-      else
-        root_class_name << c
-      end
-      prev_c = c
-    end
-
-    typedef_file = __dir__ + "/../typescript-types/events-api/#{root_class_name}.d.ts"
-    cmd = "quicktype --just-types --all-properties-optional --acronym-style original -t #{root_class_name} -l ts -o #{typedef_file}"
+    root_class_name = "Events#{filename}"
+    typedef_filepath = __dir__ + "/../typescript-types/events-api/#{root_class_name}.d.ts"
     input_json = json_file.read
-    puts "Generating #{root_class_name} from #{json_path}"
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-      stdin.write(input_json)
-    end
-
-    File.open(index_file, 'a') do |index_f|
-      index_f.puts("export { #{root_class_name} } from './events-api/#{root_class_name}';")
-    end
-
+    write(root_class_name, json_path, typedef_filepath, input_json)
+    append_to_index_ts(root_class_name, index_file, 'events-api')
   end
 end
 
 # rtm-api
 Dir.glob(__dir__ + '/../typescript-types/source/rtm-api/*').each do |json_path|
   File.open(json_path) do |json_file|
-    root_class_name = 'RTM'
-    prev_c = nil
     filename = json_path.split('/').last.gsub(/\.json$/, '')
-    filename.split('').each do |c|
-      if prev_c.nil? || prev_c == '.'
-        root_class_name << c.upcase
-      elsif c == '.'
-        # noop
-      else
-        root_class_name << c
-      end
-      prev_c = c
-    end
-
-    typedef_file = __dir__ + "/../typescript-types/rtm-api/#{root_class_name}.d.ts"
-    cmd = "quicktype --just-types --all-properties-optional --acronym-style original -t #{root_class_name} -l ts -o #{typedef_file}"
+    root_class_name = "RTM#{filename}"
+    typedef_filepath = __dir__ + "/../typescript-types/rtm-api/#{root_class_name}.d.ts"
     input_json = json_file.read
-    puts "Generating #{root_class_name} from #{json_path}"
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-      stdin.write(input_json)
-    end
-
-    File.open(index_file, 'a') do |index_f|
-      index_f.puts("export { #{root_class_name} } from './rtm-api/#{root_class_name}';")
-    end
-
+    write(root_class_name, json_path, typedef_filepath, input_json)
+    append_to_index_ts(root_class_name, index_file, 'rtm-api')
   end
 end
