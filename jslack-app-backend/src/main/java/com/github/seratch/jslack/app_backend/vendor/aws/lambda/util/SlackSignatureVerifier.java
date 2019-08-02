774db1e8e85e33d89b9a976d1795b29284cb6ae7
shubhvashisht <34352242+shubhvashisht@users.noreply.github.com>
2019-08-02 13:42:35 -0700

    Add files via upload
    added the feature of slack signed signature validation

--- jslack-app-backend/src/main/java/com/github/seratch/jslack/app_backend/vendor/aws/lambda/util/SlackSignatureVerifier.java
+++ jslack-app-backend/src/main/java/com/github/seratch/jslack/app_backend/vendor/aws/lambda/util/SlackSignatureVerifier.java
@@ -2,8 +2,14 @@ package com.github.seratch.jslack.app_backend.vendor.aws.lambda.util;
 
 import com.github.seratch.jslack.app_backend.SlackSignature;
 import com.github.seratch.jslack.app_backend.vendor.aws.lambda.request.ApiGatewayRequest;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 public class SlackSignatureVerifier {
+
+    public static final int MAX_TIME_TO_RETAIN_SLACK_REQUEST = 300000; //ms
 
     private final SlackSignature.Generator signatureGenerator;
 
@@ -22,6 +28,26 @@ public class SlackSignatureVerifier {
             String expected = signatureGenerator.generate(requestTimestamp, requestBody);
             String actual = request.getHeaders().get(SlackSignature.HeaderNames.X_SLACK_SIGNATURE);
             return actual != null && expected != null && actual.equals(expected);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isValid(HttpServletRequest request, String requestBody) {
+        if (request != null && request.getHeaderNames() != null) {
+            String requestTimestamp = request.getHeader(SlackSignature.HeaderNames.X_SLACK_REQUEST_TIMESTAMP);
+
+            Long slackTimestampInMilliSeconds =  Long.parseLong(requestTimestamp) * 1000;
+            Long currentTimeInMilliSeconds = System.currentTimeMillis();
+            if ( (currentTimeInMilliSeconds - slackTimestampInMilliSeconds) < MAX_TIME_TO_RETAIN_SLACK_REQUEST) {
+                String expected = signatureGenerator.generate(requestTimestamp, requestBody);
+                String actual = request.getHeader(SlackSignature.HeaderNames.X_SLACK_SIGNATURE);
+                return actual != null && expected != null && actual.equals(expected);
+            }
+            else{
+                log.info("REPLAY ATTACK!! Slack message is older than 5 minutes : " + slackTimestampInMilliSeconds + " Vs " + currentTimeInMilliSeconds);
+                return false;
+            }
         } else {
             return false;
         }
