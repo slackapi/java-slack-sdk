2c5370525293fc611d478bbc61d9eca837bde08b
Kazuhiro Sera <seratch@gmail.com>
2020-01-30 01:17:44 +0900

    Pass all the tests as of 2020-01-30

--- slack-api-client/src/main/java/com/slack/api/audit/Actions.java
+++ slack-api-client/src/main/java/com/slack/api/audit/Actions.java
@@ -194,4 +194,15 @@ public class Actions {
         public static final String message_restored = "message_restored";
     }
 
+    public static class WorkflowBuilder {
+        private WorkflowBuilder() {
+        }
+
+        public static final String workflow_created = "workflow_created";
+        public static final String workflow_deleted = "workflow_deleted";
+        public static final String workflow_published = "workflow_published";
+        public static final String workflow_unpublished = "workflow_unpublished";
+        public static final String workflow_responses_csv_download = "workflow_responses_csv_download";
+        public static final String workflow_unknown_action = "workflow_unknown_action";
+    }
 }
