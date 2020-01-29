253f03d32e44bcf8f83af73ddb98bb626601804d
Kazuhiro Sera <seratch@gmail.com>
2020-01-30 00:00:52 +0900

    Change groupId and enable snapshot deployment

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,6 @@
 mvn deploy \
   -P release-sign-artifacts \
   -D maven.test.skip=true \
-  -pl !jslack-lightning-kotlin-examples \
-  -pl !jslack-lightning-quarkus-examples \
-  -pl !jslack-lightning-spring-boot-examples
+  -pl !lightning-kotlin-examples \
+  -pl !lightning-quarkus-examples \
+  -pl !lightning-spring-boot-examples
