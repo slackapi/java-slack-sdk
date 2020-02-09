e5b65e59f0c87441ace586174bbc7d6e0c8b5ab6
Kazuhiro Sera <seratch@gmail.com>
2020-02-09 18:55:38 +0900

    Update UsersSelectElement
    - add the latest docs
    - remove unnecessary fields

--- slack-api-model/src/main/java/com/slack/api/model/block/element/UsersSelectElement.java
+++ slack-api-model/src/main/java/com/slack/api/model/block/element/UsersSelectElement.java
@@ -5,7 +5,7 @@ import com.slack.api.model.block.composition.PlainTextObject;
 import lombok.*;
 
 /**
- * https://api.slack.com/reference/messaging/block-elements#users-select
+ * https://api.slack.com/reference/block-kit/block-elements#users_select
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -16,8 +16,27 @@ public class UsersSelectElement extends BlockElement {
     public static final String TYPE = "users_select";
     private final String type = TYPE;
 
+    /**
+     * A plain_text only text object that defines the placeholder text shown on the menu.
+     * Maximum length for the text in this field is 150 characters.
+     */
     private PlainTextObject placeholder;
+
+    /**
+     * An identifier for the action triggered when a menu option is selected.
+     * You can use this when you receive an interaction payload to identify the source of the action.
+     * Should be unique among all other action_ids used elsewhere by your app.
+     * Maximum length for this field is 255 characters.
+     */
     private String actionId;
+
+    /**
+     * The user ID of any valid user to be pre-selected when the menu loads.
+     */
     private String initialUser;
+
+    /**
+     * A confirm object that defines an optional confirmation dialog that appears after a menu item is selected.
+     */
     private ConfirmationDialogObject confirm;
 }
