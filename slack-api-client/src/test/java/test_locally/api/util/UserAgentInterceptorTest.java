0928ce28b2e8c03d525cf943b66864fcde3afd8d
Kazuhiro Sera <seratch@gmail.com>
2020-03-05 15:57:42 +0900

    Add prefix to the User-Agent string

--- slack-api-client/src/test/java/test_locally/api/util/UserAgentInterceptorTest.java
+++ slack-api-client/src/test/java/test_locally/api/util/UserAgentInterceptorTest.java
@@ -12,12 +12,14 @@ public class UserAgentInterceptorTest {
 
     @Test
     public void defaultUserAgent() {
+        // e.g., "Java-Slack-SDK; slack-api-client/1.0.0-M3-SNAPSHOT; OpenJDK 64-Bit Server VM/11.0.5; Mac OS X/10.15.3;"
         String userAgent = UserAgentInterceptor.buildDefaultUserAgent(Collections.emptyMap());
         String[] elements = userAgent.split(";");
-        assertEquals(userAgent, 3, elements.length);
-        assertTrue(elements[0], elements[0].trim().matches("slack-api-client/.+"));
-        assertTrue(elements[1], elements[1].trim().matches("[^/]+/.+"));
+        assertEquals(userAgent, 4, elements.length);
+        assertTrue(elements[0], elements[0].trim().equals("Java-Slack-SDK"));
+        assertTrue(elements[1], elements[1].trim().matches("slack-api-client/.+"));
         assertTrue(elements[2], elements[2].trim().matches("[^/]+/.+"));
+        assertTrue(elements[3], elements[3].trim().matches("[^/]+/.+"));
     }
 
 }
