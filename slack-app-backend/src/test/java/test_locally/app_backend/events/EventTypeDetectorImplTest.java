445a4ee07c82135af1a52f5283978e388c09612c
Kazuhiro Sera <seratch@gmail.com>
2020-06-17 17:06:52 +0900

    Fix #489 Event type extraction failure in slack-app-backend (no effect to Bolt)

--- slack-app-backend/src/test/java/test_locally/app_backend/events/EventTypeDetectorImplTest.java
+++ slack-app-backend/src/test/java/test_locally/app_backend/events/EventTypeDetectorImplTest.java
@@ -12,6 +12,26 @@ public class EventTypeDetectorImplTest {
     private EventTypeExtractor eventTypeExtractor = new EventTypeExtractorImpl();
 
     @Test
+    public void notFound() {
+        {
+            String eventType = eventTypeExtractor.extractEventType("{}");
+            assertThat(eventType, is(""));
+        }
+        {
+            String eventType = eventTypeExtractor.extractEventType("{\"event\": {}}");
+            assertThat(eventType, is(""));
+        }
+        {
+            String eventType = eventTypeExtractor.extractEventSubtype("{}");
+            assertThat(eventType, is(""));
+        }
+        {
+            String eventType = eventTypeExtractor.extractEventSubtype("{\"event\": {}}");
+            assertThat(eventType, is(""));
+        }
+    }
+
+    @Test
     public void detect_goodbye() {
         String payload = "{\n" +
                 "        \"token\": \"XXYYZZ\",\n" +
@@ -49,6 +69,51 @@ public class EventTypeDetectorImplTest {
                 "        ],\n" +
                 "        \"event_id\": \"Ev08MFMKH6\",\n" +
                 "        \"event_time\": 1234567890\n" +
+                "}";
+        String eventType = eventTypeExtractor.extractEventType(payload);
+        assertThat(eventType, is("message"));
+    }
+
+    @Test
+    public void issue_489() {
+        String payload = "{\n" +
+                "  \"authed_users\": [\n" +
+                "    \"RETRACTED\"\n" +
+                "  ],\n" +
+                "  \"event_id\": \"RETRACTED\",\n" +
+                "  \"api_app_id\": \"RETRACTED\",\n" +
+                "  \"team_id\": \"RETRACTED\",\n" +
+                "  \"event\": {\n" +
+                "    \"client_msg_id\": \"b5ac4998-4526-4d0b-8bb0-7bd614c1b774\",\n" +
+                "    \"blocks\": [\n" +
+                "      {\n" +
+                "        \"elements\": [\n" +
+                "          {\n" +
+                "            \"elements\": [\n" +
+                "              {\n" +
+                "                \"text\": \"test\",\n" +
+                "                \"type\": \"text\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"type\": \"rich_text_section\"\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"type\": \"rich_text\",\n" +
+                "        \"block_id\": \"Ws3Y\"\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"event_ts\": \"1592257234.003200\",\n" +
+                "    \"channel\": \"RETRACTED\",\n" +
+                "    \"text\": \"test\",\n" +
+                "    \"team\": \"RETRACTED\",\n" +
+                "    \"type\": \"message\",\n" +
+                "    \"channel_type\": \"channel\",\n" +
+                "    \"user\": \"RETRACTED\",\n" +
+                "    \"ts\": \"1592257234.003200\"\n" +
+                "  },\n" +
+                "  \"type\": \"event_callback\",\n" +
+                "  \"event_time\": 1592257234,\n" +
+                "  \"token\": \"RETRACTED\"\n" +
                 "}";
         String eventType = eventTypeExtractor.extractEventType(payload);
         assertThat(eventType, is("message"));
