d0a756f3ef35d3510db3a15fc29ccefc062fc483
Kazuhiro Sera <seratch@gmail.com>
2020-09-02 00:03:29 +0900

    Fix #498 by adding Google Cloud Functions support

---
+++ bolt-google-cloud-functions-example/src/main/java/functions/HelloSlack.java
@@ -0,0 +1,30 @@
+package functions;
+
+import com.slack.api.bolt.App;
+
+// export SLACK_BOT_TOKEN=
+// export SLACK_SIGNING_SECRET=
+
+// Local Development
+// mvn function:run
+// ngrok http 8080 --subdomain your-domain
+
+// Deployment
+// gcloud functions deploy my-first-function --entry-point functions.HelloSlack --runtime java11 --trigger-http --memory 512MB --allow-unauthenticated --set-env-vars SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN,SLACK_SIGNING_SECRET=$SLACK_SIGNING_SECRET
+
+/**
+ * Refer to https://cloud.google.com/functions/docs/first-java for details.
+ */
+public class HelloSlack extends SlackApiFunction {
+    private static final App app = new App();
+
+    static {
+        app.command("/hello-google-cloud-functions", (req, ctx) -> {
+            return ctx.ack("Hi from Google Cloud Functions!");
+        });
+    }
+
+    public HelloSlack() {
+        super(app);
+    }
+}
--- scripts/run_all_bolt_tests.sh
+++ scripts/run_all_bolt_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./mvnw install -Dmaven.test.skip=true && \
-./mvnw test -pl bolt -pl slack-app-backend -pl bolt-servlet -pl bolt-aws-lambda -pl bolt-micronaut -pl bolt-helidon
+./mvnw test -pl bolt -pl slack-app-backend -pl bolt-servlet -pl bolt-aws-lambda -pl bolt-micronaut -pl bolt-helidon -pl bolt-google-cloud-functions
