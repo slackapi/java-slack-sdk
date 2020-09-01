c6e0b1cbaaceea0aa642cbd5036d63054698b736
Kazuhiro Sera <seratch@gmail.com>
2020-09-02 00:46:25 +0900

    Fix builds

--- scripts/run_no_prep_tests.sh
+++ scripts/run_no_prep_tests.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-./mvnw ${MAVEN_OPTS} \
-  clean \
-  test-compile \
-  '-Dtest=test_locally.**.*Test' test \
-  -DfailIfNoTests=false \
-  --no-transfer-progress && \
-  if git status --porcelain | grep .; then git --no-pager diff; exit 1; fi
+is_jdk_8=`echo $JAVA_HOME | grep openjdk-8.jdk`
+if [[ "${is_jdk_8}" != "" ]];
+then
+  ./mvnw ${MAVEN_OPTS} -pl !bolt-google-cloud-functions \
+    clean \
+    test-compile \
+    '-Dtest=test_locally.**.*Test' test \
+    -DfailIfNoTests=false \
+    --no-transfer-progress && \
+    if git status --porcelain | grep .; then git --no-pager diff; exit 1; fi
+else
+  ./mvnw ${MAVEN_OPTS} \
+    clean \
+    test-compile \
+    '-Dtest=test_locally.**.*Test' test \
+    -DfailIfNoTests=false \
+    --no-transfer-progress && \
+    if git status --porcelain | grep .; then git --no-pager diff; exit 1; fi
+fi
