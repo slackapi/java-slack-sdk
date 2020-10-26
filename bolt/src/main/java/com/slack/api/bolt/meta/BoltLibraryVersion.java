e0021aedd7643a40a1d475d34a682ecfac4fbabe
Joshua Segal <gitpulljoshuasegal@gmail.com>
2020-10-26 12:02:53 -0400

    Update BoltLibraryVersion.java

--- bolt/src/main/java/com/slack/api/bolt/meta/BoltLibraryVersion.java
+++ bolt/src/main/java/com/slack/api/bolt/meta/BoltLibraryVersion.java
@@ -1,11 +1,9 @@
 package com.slack.api.bolt.meta;
 
 public final class BoltLibraryVersion {
+    public static final String VERSION = "1.3.0-SNAPSHOT";
+    
     private BoltLibraryVersion() {
-    }
-
-    public static final String get() {
-        return "1.3.0-SNAPSHOT";
     }
 }
 
