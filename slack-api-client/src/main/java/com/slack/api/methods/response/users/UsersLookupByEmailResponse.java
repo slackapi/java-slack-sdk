6d4f496b98cda4667dca67dfb7612d2ed662e882
Kazuhiro Sera <seratch@gmail.com>
2020-11-12 18:54:08 +0900

    Fix #614 by overriding methods

--- slack-api-client/src/main/java/com/slack/api/methods/response/users/UsersLookupByEmailResponse.java
+++ slack-api-client/src/main/java/com/slack/api/methods/response/users/UsersLookupByEmailResponse.java
@@ -1,4 +1,89 @@
 package com.slack.api.methods.response.users;
 
+import com.slack.api.model.User;
+
 public class UsersLookupByEmailResponse extends com.slack.api.methods.response.channels.UsersLookupByEmailResponse {
+    public UsersLookupByEmailResponse() {
+        super();
+    }
+
+    @Override
+    public boolean isOk() {
+        return super.isOk();
+    }
+
+    @Override
+    public String getWarning() {
+        return super.getWarning();
+    }
+
+    @Override
+    public String getError() {
+        return super.getError();
+    }
+
+    @Override
+    public String getNeeded() {
+        return super.getNeeded();
+    }
+
+    @Override
+    public String getProvided() {
+        return super.getProvided();
+    }
+
+    @Override
+    public User getUser() {
+        return super.getUser();
+    }
+
+    @Override
+    public void setOk(boolean ok) {
+        super.setOk(ok);
+    }
+
+    @Override
+    public void setWarning(String warning) {
+        super.setWarning(warning);
+    }
+
+    @Override
+    public void setError(String error) {
+        super.setError(error);
+    }
+
+    @Override
+    public void setNeeded(String needed) {
+        super.setNeeded(needed);
+    }
+
+    @Override
+    public void setProvided(String provided) {
+        super.setProvided(provided);
+    }
+
+    @Override
+    public void setUser(User user) {
+        super.setUser(user);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    protected boolean canEqual(Object other) {
+        return super.canEqual(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
