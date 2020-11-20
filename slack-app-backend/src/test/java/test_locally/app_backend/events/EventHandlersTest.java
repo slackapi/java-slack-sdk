ef0b8448187b24f3fc580d39ca40dd91ba8e7163
Gaspard Petit <gpetit@eidosmontreal.com>
2020-11-20 02:16:13 -0500

    Squashed commit of the following:
    commit ec23d182b4713d438a4c702ed523e7efe36dfdcf
    Merge: 67848047 7b5f18f1
    Author: Gaspard Petit <gpetit@eidosmontreal.com>
    Date:   Fri Nov 20 01:57:56 2020 -0500
    
        Merge branch 'support_topic_change_events' of https://github.com/eidosmontreal/java-slack-sdk into support_topic_change_events
    
    commit 678480479853baf3acb4620c00b2194743c240e7
    Author: Gaspard Petit <gpetit@eidosmontreal.com>
    Date:   Wed Sep 16 22:29:18 2020 -0400
    
        Adding support for channel_topic and group_topic events
    
        These events are posted when the topic of a public conversation and a private conversation is changed respectively
    
    commit 7b5f18f1461f7d31c3e5ba3dbd668f8d6766f753
    Merge: cdfe6eda 4768d2f3
    Author: Gaspard Petit <gpetit@eidosmontreal.com>
    Date:   Fri Nov 20 01:27:57 2020 -0500
    
        Merge branch 'support_topic_change_events' of https://github.com/eidosmontreal/java-slack-sdk into support_topic_change_events
    
        # Conflicts:
        #	slack-app-backend/src/main/java/com/slack/api/app_backend/events/payload/ChannelTopicMessagePayload.java
        #	slack-app-backend/src/main/java/com/slack/api/app_backend/events/payload/GroupTopicMessagePayload.java
    
    commit cdfe6edab5b8906cb2c150f68b593271ace3101c
    Author: Gaspard Petit <gpetit@eidosmontreal.com>
    Date:   Fri Nov 20 01:24:38 2020 -0500
    
        Updated to 1.4
    
        Updated branch to 1.4; fixed failing unit test
    
    commit 63c2cef87c9d3446d1d68d065d599b26646ab85b
    Author: Gaspard Petit <gpetit@eidosmontreal.com>
    Date:   Wed Sep 16 22:29:18 2020 -0400
    
        Adding support for channel_topic and group_topic events
    
        These events are posted when the topic of a public conversation and a private conversation is changed respectively
    
    commit 4768d2f30311752a5234a277737c6e5038fc3544
    Author: Gaspard Petit <gpetit@eidosmontreal.com>
    Date:   Wed Sep 16 22:29:18 2020 -0400
    
        Adding support for channel_topic and group_topic events
    
        These events are posted when the topic of a public conversation and a private conversation is changed respectively

--- slack-app-backend/src/test/java/test_locally/app_backend/events/EventHandlersTest.java
+++ slack-app-backend/src/test/java/test_locally/app_backend/events/EventHandlersTest.java
@@ -15,7 +15,7 @@ public class EventHandlersTest {
 
     @Test
     public void handlers() {
-        List<EventHandler<?>> handlers = Arrays.asList(
+        List<EventHandler<?>> handlersWithoutSubtype = Arrays.asList(
                 new ImCloseHandler() {
                     @Override
                     public void handle(ImClosePayload payload) {
@@ -335,11 +335,40 @@ public class EventHandlersTest {
                     @Override
                     public void handle(CallRejectedPayload payload) {
                     }
+                },
+                new GroupTopicMessageHandler() {
+                    @Override
+                    public void handle(GroupTopicMessagePayload payload) {
+                    }
+                },
+                new ChannelTopicMessageHandler() {
+                    @Override
+                    public void handle(ChannelTopicMessagePayload payload) {
+                    }
                 }
         );
-        for (EventHandler<?> handler : handlers) {
+
+        for (EventHandler<?> handler : handlersWithoutSubtype) {
             assertNotNull(handler.getEventType());
             assertNull(handler.getEventSubtype());
+        }
+
+        List<EventHandler<?>> handlersWithSubtype = Arrays.asList(
+                new GroupTopicMessageHandler() {
+                    @Override
+                    public void handle(GroupTopicMessagePayload payload) {
+                    }
+                },
+                new ChannelTopicMessageHandler() {
+                    @Override
+                    public void handle(ChannelTopicMessagePayload payload) {
+                    }
+                }
+        );
+
+        for (EventHandler<?> handler : handlersWithSubtype) {
+            assertNotNull(handler.getEventType());
+            assertNotNull(handler.getEventSubtype());
         }
     }
 
