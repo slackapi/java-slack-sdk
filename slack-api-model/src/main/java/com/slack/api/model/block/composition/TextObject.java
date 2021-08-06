4a9f5974adb1e829923b33c0dca0d2c344b041f8
Kazuhiro Sera <ksera@slack-corp.com>
2021-08-06 22:00:53 +0900

    Fix #759 Add abstract getText() method to the TextObject class (#801)

--- slack-api-model/src/main/java/com/slack/api/model/block/composition/TextObject.java
+++ slack-api-model/src/main/java/com/slack/api/model/block/composition/TextObject.java
@@ -6,4 +6,7 @@ import com.slack.api.model.block.ContextBlockElement;
  * https://api.slack.com/reference/messaging/composition-objects#text
  */
 public abstract class TextObject implements ContextBlockElement {
+
+    public abstract String getText();
+
 }
