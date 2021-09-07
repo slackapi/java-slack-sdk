95ea765f1daa963e928521f620a70192d6943385
Kazuhiro Sera <seratch@gmail.com>
2021-09-07 13:23:03 +0900

    Add more tests

--- bolt-google-cloud-functions/src/test/java/test_locally/ResponseUtilityTest.java
+++ bolt-google-cloud-functions/src/test/java/test_locally/ResponseUtilityTest.java
@@ -1,13 +1,73 @@
 package test_locally;
 
+import com.google.cloud.functions.HttpResponse;
 import com.slack.api.bolt.response.Response;
 import org.junit.Test;
 
+import java.io.*;
+import java.util.*;
+
 import static com.slack.api.bolt.google_cloud_functions.SlackApiFunction.buildNotNullResponseBody;
+import static com.slack.api.bolt.google_cloud_functions.SlackApiFunction.writeResponse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ResponseUtilityTest {
+    private static HttpResponse buildNullHttpResponse() {
+        return new HttpResponse() {
+            @Override
+            public void setStatusCode(int code) {
+            }
+
+            @Override
+            public void setStatusCode(int code, String message) {
+            }
+
+            @Override
+            public void setContentType(String contentType) {
+            }
+
+            @Override
+            public Optional<String> getContentType() {
+                return Optional.empty();
+            }
+
+            @Override
+            public void appendHeader(String header, String value) {
+            }
+
+            @Override
+            public Map<String, List<String>> getHeaders() {
+                return null;
+            }
+
+            @Override
+            public OutputStream getOutputStream() throws IOException {
+                return new ByteArrayOutputStream();
+            }
+
+            @Override
+            public BufferedWriter getWriter() throws IOException {
+                return new BufferedWriter(new StringWriter());
+            }
+        };
+    }
+
+    @Test
+    public void writeResponse_null_headers() throws IOException {
+        Response response = Response.builder().statusCode(200).body(null).headers(null).build();
+        HttpResponse httpResponse = buildNullHttpResponse();
+        writeResponse(response, httpResponse);
+    }
+
+    @Test
+    public void writeResponse_non_null_values() throws IOException {
+        Map<String, List<String>> headers = new HashMap<>();
+        headers.put("Set-Cookie", Arrays.asList("1", "2"));
+        Response response = Response.builder().statusCode(200).body("foo").headers(headers).build();
+        HttpResponse httpResponse = buildNullHttpResponse();
+        writeResponse(response, httpResponse);
+    }
 
     @Test
     public void toNotNullResponseBody_null() {
