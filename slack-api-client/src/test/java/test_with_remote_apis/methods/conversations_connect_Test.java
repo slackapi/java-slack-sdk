d7a53bcb5a921db291b7b1cd485852d88731f0a3
Kazuhiro Sera <seratch@gmail.com>
2021-11-24 07:45:02 +0900

    Run all the integration tests - 2021-11-23 PT

--- slack-api-client/src/test/java/test_with_remote_apis/methods/conversations_connect_Test.java
+++ slack-api-client/src/test/java/test_with_remote_apis/methods/conversations_connect_Test.java
@@ -95,8 +95,7 @@ public class conversations_connect_Test {
             ConversationsDeclineSharedInviteResponse decline = receiver.conversationsDeclineSharedInvite(r -> r
                     .inviteId(invitation.getInviteId())
             );
-            // TODO: Getting "internal_error" (since 2021-11-10)
-            // assertThat(decline.getError(), is(nullValue()));
+            assertThat(decline.getError(), is(nullValue()));
 
         } finally {
             ConversationsArchiveResponse archive = sender.conversationsArchive(r -> r.channel(channelId));
