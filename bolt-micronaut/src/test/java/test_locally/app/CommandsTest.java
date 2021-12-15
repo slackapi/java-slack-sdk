2714e1df9d3645a589fb35ada954dfdcb2e40659
Kazuhiro Sera <seratch@gmail.com>
2021-12-15 15:48:44 +0900

    Fix #891 Migrate Micronaut version to 3.2

--- bolt-micronaut/src/test/java/test_locally/app/CommandsTest.java
+++ bolt-micronaut/src/test/java/test_locally/app/CommandsTest.java
@@ -11,15 +11,15 @@ import io.micronaut.context.annotation.Primary;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MutableHttpRequest;
-import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.rxjava3.http.client.Rx3HttpClient;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
 import java.io.IOException;
 
 import static org.mockito.Mockito.*;
@@ -43,7 +43,7 @@ public class CommandsTest {
 
     @Inject
     @Client("/")
-    RxHttpClient client;
+    Rx3HttpClient client;
 
     String signingSecret = "test";
     String botToken = "xoxb-dummy";
