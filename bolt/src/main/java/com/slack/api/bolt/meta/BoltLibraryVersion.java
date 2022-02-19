81e766175ff0d33e8db17db0f0784faa98f7c8d5
Kazuhiro Sera <seratch@gmail.com>
2022-02-19 10:05:22 +0900

    Start new development

--- bolt/src/main/java/com/slack/api/bolt/meta/BoltLibraryVersion.java
+++ bolt/src/main/java/com/slack/api/bolt/meta/BoltLibraryVersion.java
@@ -5,7 +5,7 @@ public final class BoltLibraryVersion {
     }
 
     public static final String get() {
-        return "1.20.0";
+        return "1.20.1-SNAPSHOT";
     }
 }
 
--- slack-api-model/src/main/java/com/slack/api/meta/SlackApiModelLibraryVersion.java
+++ slack-api-model/src/main/java/com/slack/api/meta/SlackApiModelLibraryVersion.java
@@ -5,7 +5,7 @@ public final class SlackApiModelLibraryVersion {
     }
 
     public static final String get() {
-        return "1.20.0";
+        return "1.20.1-SNAPSHOT";
     }
 }
 
