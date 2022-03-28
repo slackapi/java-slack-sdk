312b6483548b462bba548bb9331a52d157151835
Kazuhiro Sera <seratch@gmail.com>
2022-03-28 09:08:40 +0900

    Fix #954 Add setToken(String) to MethodsClient request Interface (#957)

--- slack-api-client/src/main/java/com/slack/api/methods/SlackApiRequest.java
+++ slack-api-client/src/main/java/com/slack/api/methods/SlackApiRequest.java
@@ -2,7 +2,7 @@ package com.slack.api.methods;
 
 /**
  * A marker interface for Slack API request objects.
- *
+ * <p>
  * Developers can instantiate a request object by either of the following ways:
  * <code>
  * AuthTestRequest req = AuthTestRequest.builder().token("your-token").build();
@@ -17,6 +17,25 @@ package com.slack.api.methods;
  */
 public interface SlackApiRequest {
 
+    /**
+     * Returns a token in this request object.
+     * If the API endpoint does not require a token (e.g., api.test), this method can return null.
+     *
+     * @return token string value or null
+     */
     String getToken();
+
+    /**
+     * Updates the token in this request object.
+     * <p>
+     * The default implementation throws {@link UnsupportedOperationException}.
+     * All the built-in implementing classes overrides this method in a proper way.
+     *
+     * @throws UnsupportedOperationException is always thrown if this method is not overridden
+     */
+    default void setToken(String token) {
+        throw new UnsupportedOperationException(
+                "SlackApiRequest#setToken(String) is not overridden in this class.");
+    }
 
 }
---
+++ slack-api-client/src/test/java/test_locally/api/methods/SlackApiRequestTest.java
@@ -0,0 +1,69 @@
+package test_locally.api.methods;
+
+import com.slack.api.Slack;
+import com.slack.api.SlackConfig;
+import com.slack.api.methods.SlackApiRequest;
+import com.slack.api.methods.request.auth.AuthTestRequest;
+import com.slack.api.methods.response.auth.AuthTestResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import util.MockSlackApiServer;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static util.MockSlackApi.ExpiredToken;
+import static util.MockSlackApi.ValidToken;
+
+public class SlackApiRequestTest {
+
+    MockSlackApiServer server = new MockSlackApiServer();
+    SlackConfig config = new SlackConfig();
+    Slack slack = Slack.getInstance(config);
+
+    @Before
+    public void setup() throws Exception {
+        server.start();
+        config.setMethodsEndpointUrlPrefix(server.getMethodsEndpointPrefix());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void defaultSetToken() {
+        SlackApiRequest req = new SlackApiRequest() {
+            @Override
+            public String getToken() {
+                return null;
+            }
+        };
+        req.setToken("foo");
+    }
+
+    @Test
+    public void tokenReplacement() {
+        AuthTestRequest req = AuthTestRequest.builder().token("xoxb-old-token").build();
+        assertThat(req.getToken(), is("xoxb-old-token"));
+        req.setToken("xoxb-new-token"); // no exception here
+        assertThat(req.getToken(), is("xoxb-new-token"));
+    }
+
+    @Test
+    public void setTokenUseCase() throws Exception {
+        AuthTestRequest req = AuthTestRequest.builder().token(ExpiredToken).build();
+        AuthTestResponse response1 = slack.methods().authTest(req);
+        assertThat(response1.getError(), is("token_expired"));
+
+        replaceToken(req, ValidToken);
+        AuthTestResponse response2 = slack.methods().authTest(req);
+        assertThat(response2.getError(), is(nullValue()));
+    }
+
+    private static void replaceToken(SlackApiRequest req, String newToken) {
+        req.setToken(newToken);
+    }
+
+}
