fface8001cd1348a7611a746aca1b057d30d33af
Kazuhiro Sera <seratch@gmail.com>
2022-05-10 06:58:36 +0900

    Update slack-api-model/src/main/java/com/slack/api/model/event/UserHuddleChangedEvent.java
    Co-authored-by: Michael Brooks <michael@michaelbrooks.ca>

--- slack-api-model/src/main/java/com/slack/api/model/event/UserHuddleChangedEvent.java
+++ slack-api-model/src/main/java/com/slack/api/model/event/UserHuddleChangedEvent.java
@@ -4,7 +4,7 @@ import com.slack.api.model.User;
 import lombok.Data;
 
 /**
- * The user_huddle_changed event is sent to all connections for a workspace when a users's huddle status is changed.
+ * The user_huddle_changed event is sent to all connections for a workspace when a user's huddle status is changed.
  * The event is identical to the existing user_change event. Both user_change and user_huddle_changed are dispatched
  * at the exact same time. Use the users.profile.set method to update user profile data.
  * <p>
