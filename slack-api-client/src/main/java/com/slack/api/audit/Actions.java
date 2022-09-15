19173d5a8be240243cad27b2040a3b04894b7adc
Kazuhiro Sera <seratch@gmail.com>
2022-09-16 07:15:00 +0900

    Run all the integration tests - 2022-09-15 PT

--- slack-api-client/src/main/java/com/slack/api/audit/Actions.java
+++ slack-api-client/src/main/java/com/slack/api/audit/Actions.java
@@ -119,6 +119,7 @@ public class Actions {
         public static final String pref_uneditable_user_profile_fields = "pref.uneditable_user_profile_fields";
         public static final String pref_ext_audit_log_retention_type_changed = "pref.ext_audit_log_retention_type_changed";
         public static final String pref_ext_audit_log_retention_duration_changed = "pref.ext_audit_log_retention_duration_changed";
+        public static final String pref_canvas_changed = "pref.canvas_changed";
         public static final String manual_export_downloaded = "manual_export_downloaded";
         public static final String manual_export_deleted = "manual_export_deleted";
         public static final String scheduled_export_downloaded = "scheduled_export_downloaded";
