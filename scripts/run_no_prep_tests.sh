46cf9b2d1efedb0ee63e1cb8954d31089efc883f
William Bergamin <wbergamin@slack-corp.com>
2022-10-28 12:19:06 -0400

    Removed deprecated CodCov script
    improved CI build and removed deprecated codcov script

--- scripts/run_no_prep_tests.sh
+++ scripts/run_no_prep_tests.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+flags()
+{
+    while test $# -gt 0
+    do
+        case "$1" in
+        -ci)
+            CI_OPTIONS="--batch-mode -T 1C"
+            ;;
+        *) usage;;
+        esac
+        shift
+    done
+}
+flags "$@"
+
 is_jdk_8=`echo $JAVA_HOME | grep 8.`
 is_travis_jdk_8=`echo $TRAVIS_JDK | grep openjdk8`
 if [[ "${is_jdk_8}" != "" && "${is_travis_jdk_8}" != "" ]];
@@ -20,7 +36,7 @@ else
   ./mvnw ${MAVEN_OPTS} \
     clean \
     test-compile \
-    '-Dtest=test_locally.**.*Test' test \
+    '-Dtest=test_locally.**.*Test' test ${CI_OPTIONS} \
     -DfailIfNoTests=false \
     -Dhttps.protocols=TLSv1.2 \
     --no-transfer-progress && \
