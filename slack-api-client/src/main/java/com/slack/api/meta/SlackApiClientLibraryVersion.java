81f53dfd1a3e9cc544ca4f8970e6c41f0bce7c0a
Kazuhiro Sera <seratch@gmail.com>
2023-03-08 15:55:10 +0900

    version 1.28.0

--- slack-api-client/src/main/java/com/slack/api/meta/SlackApiClientLibraryVersion.java
+++ slack-api-client/src/main/java/com/slack/api/meta/SlackApiClientLibraryVersion.java
@@ -5,7 +5,7 @@ public final class SlackApiClientLibraryVersion {
     }
 
     public static final String get() {
-        return "1.27.4-SNAPSHOT";
+        return "1.28.0";
     }
 }
 
--- slack-api-model/src/main/java/com/slack/api/meta/SlackApiModelLibraryVersion.java
+++ slack-api-model/src/main/java/com/slack/api/meta/SlackApiModelLibraryVersion.java
@@ -5,7 +5,7 @@ public final class SlackApiModelLibraryVersion {
     }
 
     public static final String get() {
-        return "1.27.4-SNAPSHOT";
+        return "1.28.0";
     }
 }
 
