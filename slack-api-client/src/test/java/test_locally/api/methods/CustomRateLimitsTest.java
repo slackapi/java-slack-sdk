ff54ca59bdee240ca2e6acb21bb2fe3d7dc39e28
Kazuhiro Sera <seratch@gmail.com>
2023-04-03 17:57:40 +0900

    Adjust

--- slack-api-client/src/test/java/test_locally/api/methods/CustomRateLimitsTest.java
+++ slack-api-client/src/test/java/test_locally/api/methods/CustomRateLimitsTest.java
@@ -66,7 +66,7 @@ public class CustomRateLimitsTest {
         // With the default settings, this call is on the "safe" pace, so spentMillis will be 15L or so
         client.authTest(r -> r).get();
         long spentMillis = System.currentTimeMillis() - startMillis;
-        assertThat(spentMillis, is(greaterThan(60_000L / customLimit * 2)));
+        assertThat(spentMillis, is(greaterThan(1000L )));
     }
 
 }
