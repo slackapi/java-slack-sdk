93f5e3d91a3fdc15ac199f669dfd1b963ddf49ab
Kazuhiro Sera <seratch@gmail.com>
2023-05-23 15:50:56 +0900

    Uprade optional dependencies

--- scripts/check_dependency_updates.sh
+++ scripts/check_dependency_updates.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-./mvnw -pl !bolt-quarkus-examples versions:display-dependency-updates | grep -v checking
+./mvnw -pl !bolt-quarkus-examples versions:display-dependency-updates | \
+  grep -v checking | \
+  grep -v org.mockito:mockito-core | \
+  grep -v org.eclipse.jetty | \
+  grep -v javax.servlet:javax.servlet-api | \
+  grep -v jakarta.servlet:jakarta.servlet-api | \
+  grep -v ch.qos.logback:logback-classic | \
+  grep -v org.glassfish.tyrus.bundles:tyrus-standalone-client | \
+  grep -v org.slf4j: | \
+  grep -v io.helidon.
