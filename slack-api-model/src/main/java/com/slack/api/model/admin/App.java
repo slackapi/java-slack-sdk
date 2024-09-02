17a68dca74016a859ca641cb9338b0bc935254d1
Kazuhiro Sera <seratch@gmail.com>
2024-09-02 11:42:52 +0900

    Fix

--- slack-api-model/src/main/java/com/slack/api/model/admin/App.java
+++ slack-api-model/src/main/java/com/slack/api/model/admin/App.java
@@ -12,11 +12,12 @@ public class App {
     private String privacyPolicyUrl;
     private String appHomepageUrl;
     private String appDirectoryUrl;
-    @SerializedName("is_granular_bot_app")
     @SerializedName("is_app_directory_approved")
     private boolean appDirectoryApproved;
     @SerializedName("is_internal")
     private boolean internal;
+    @SerializedName("is_granular_bot_app")
+    private boolean granularBotApp;
     private String additionalInfo;
     private AppIcons icons;
 }
