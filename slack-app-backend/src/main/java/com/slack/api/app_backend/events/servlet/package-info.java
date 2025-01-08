a21e1c472d1287664e246b29e773167026a111e3
Kazuhiro Sera <seratch@gmail.com>
2025-01-09 07:46:44 +0900

    Remove long-deprecated javax.servlet modules from slack-app-backend library (#1412)

--- slack-app-backend/src/main/java/com/slack/api/app_backend/events/servlet/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Using the classes under this package is no longer recommended.
- * Please consider going with bolt-servlet for new app development.
- */
-package com.slack.api.app_backend.events.servlet;
