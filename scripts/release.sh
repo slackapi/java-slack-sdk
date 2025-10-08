15958194854599b8a61c969fb622a9a9c0dd583d
William Bergamin <wbergamin@salesforce.com>
2025-10-08 12:45:06 -0400

    feat: support snapshot releases in release process (#1514)
    * feat: support snapshot releases in release process
    
    * Update phrasing

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-is_jdk_8=`echo $JAVA_HOME | grep 8.`
-is_travis_jdk_8=`echo $TRAVIS_JDK | grep openjdk8`
+is_jdk_8=$(echo "$JAVA_HOME" | grep 8.)
+is_travis_jdk_8=$(echo "$TRAVIS_JDK" | grep openjdk8)
 if [[ "${is_jdk_8}" != "" && "${is_travis_jdk_8}" != "" ]];
 then
   echo "Please use OpenJDK 11 for releasing these libraries."
@@ -10,19 +10,29 @@ fi
 
 exclusion="-pl !bolt-kotlin-examples -pl !bolt-quarkus-examples"
 
-dir=`dirname $0`/..
-release_version=`sed -n 's/<version>\([^\$]\..*\)<\/version>$/\1/p' < ${dir}/pom.xml`
+dir=$(dirname "$0")/..
+release_version=$(sed -n 's/^[[:space:]]*<version>\([^\$]\..*\)<\/version>[[:space:]]*$/\1/p' < "${dir}"/pom.xml)
 
 export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
 
 if [[ "${release_version}" =~ ^.+-SNAPSHOT$ ]]; then
-  profile=snapshot-releases
+  echo "🚀 Releasing a SNAPSHOT version ($release_version) of the project."
+  profile=production-releases
   mvn clean \
     deploy \
-    -P snapshot-releases \
+    -P production-releases \
     -D maven.test.skip=true \
     ${exclusion}
 else
+  echo "You are releasing a stable version ($release_version) of the project."
+  read -r -p "This will be available publicly. Is this correct? (y/N): " confirmation
+
+  if [[ ! "$confirmation" =~ ^[Yy]$ ]]; then
+    echo "Release cancelled by user!"
+    exit 0
+  fi
+  echo "Confirmed. Proceeding with the stable release."
+
   profile=production-releases
   mvn clean \
     deploy \
