43ea7047ebb1ed2b4501acc1f0c4e8625cc57a9d
Kirill Meshkov <meshkob@rambler.ru>
2026-03-25 06:51:25 +0000

    revert import changes

--- slack-api-client/src/test/java/test_with_remote_apis/methods/canvases_Test.java
+++ slack-api-client/src/test/java/test_with_remote_apis/methods/canvases_Test.java
@@ -25,7 +25,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @Slf4j
