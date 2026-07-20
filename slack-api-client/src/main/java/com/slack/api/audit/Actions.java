7c5298df061cbe9bd6bb43aef61e3976539051bf
Eden Zimbelman <eden.zimbelman@salesforce.com>
2026-07-20 12:41:11 -0700

    feat: add app_mcp_server_acl_changed audit action

--- slack-api-client/src/main/java/com/slack/api/audit/Actions.java
+++ slack-api-client/src/main/java/com/slack/api/audit/Actions.java
@@ -569,6 +569,7 @@ public class Actions {
         public static final String app_mcp_server_added = "app_mcp_server_added";
         public static final String app_mcp_server_updated = "app_mcp_server_updated";
         public static final String app_mcp_server_removed = "app_mcp_server_removed";
+        public static final String app_mcp_server_acl_changed = "app_mcp_server_acl_changed";
     }
 
     public static class Message {
